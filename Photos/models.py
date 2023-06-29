d5c63d9b9f21bbcf40e20180e84d4dcff5da15c3
Angeldahal <angeldahal202@gmail.com>
2023-06-29 17:01:49 +0545

    Add template website and create model to add photo

--- Photos/models.py
+++ Photos/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class Photo(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, default=1)
+    image = models.ImageField(null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+    date_uploaded = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.description
